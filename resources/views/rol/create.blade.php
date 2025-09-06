23c335d3b58daf36000f47324cafd385f26a25e0
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-09-06 15:56:29 -0600

    first commit

---
+++ resources/views/rol/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('template_title')
+    {{ __('Crear') }} Rol
+@endsection
+
+@section('content')
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="card card-default">
+                    <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
+                        <div class="float-left">
+                            <span class="card-title">{{ __('Crear') }} Rol</span>
+                        </div>
+                        <div class="float-right">
+                            <a class="btn btn-primary btn-sm" href="{{ route('rols.index') }}"> {{ __('Regresar') }}</a>
+                        </div>
+                    </div>
+                    
+                    <div class="card-body bg-white">
+                        <form method="POST" action="{{ route('rols.store') }}"  role="form" enctype="multipart/form-data">
+                            @csrf
+
+                            @include('rol.form')
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
